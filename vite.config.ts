3b9747def02a08c907e6408aeb78ccbbdb4489e7
uihyeon.cha <uihyeon.cha@gmail.com>
2022-01-16 20:25:45 +0900

    chore: vite.config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig(() => ({}))
+export default defineConfig(() => ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+            },
+        },
+        // lib: {
+        //     entry: path.resolve(__dirname, 'lib/main.js'),
+        //     name: 'MyLib',
+        //     fileName: (format) => `my-lib.${format}.js`,
+        // },
+    },
+}))
