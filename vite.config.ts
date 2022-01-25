081826942510bac290780eaf6130e3bc22ccd00b
uihyeon.cha <uihyeon.cha@gmail.com>
2022-01-25 10:23:27 +0900

    refactor: vite 설정 파일 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,77 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
+import { version } from './package.json'
+
+const definedModes = ['chrome.background', 'chrome.content'] as const
+
+type Mode = typeof definedModes[number]
 
 const rootDir = (...paths: string[]) => path.resolve(__dirname, ...paths)
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: rootDir('index.html'),
+export default defineConfig(({ command, mode }) => {
+    const typedMode = mode as Mode
+
+    if (command === 'build' && definedModes.includes(typedMode)) {
+        return getChromeConfig(typedMode)
+    }
+
+    return getDefaultConfig()
+})
+
+function getDefaultConfig(): UserConfig {
+    return {
+        build: {
+            rollupOptions: {
+                input: {
+                    main: rootDir('index.html'),
+                },
             },
         },
-    },
-    resolve: {
-        alias: {
-            '~': rootDir('./src'),
+        resolve: {
+            alias: {
+                '~': rootDir('./src'),
+            },
         },
-    },
-})
+    }
+}
+
+function getChromeConfig(mode: Mode): UserConfig {
+    let inputKey: string
+    let inputValue: string
+
+    switch (mode) {
+        case 'chrome.background': {
+            inputKey = 'background'
+            inputValue = rootDir('src/chrome/background.ts')
+            break
+        }
+
+        case 'chrome.content': {
+            inputKey = 'content'
+            inputValue = rootDir('src/chrome/content.ts')
+            break
+        }
+    }
+
+    return {
+        mode: 'production',
+        build: {
+            outDir: `release/text-picker@${version}`,
+            emptyOutDir: false,
+            rollupOptions: {
+                input: {
+                    [inputKey]: inputValue,
+                },
+                output: {
+                    format: 'umd',
+                    entryFileNames: '[name].js',
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                '~': rootDir('./src'),
+            },
+        },
+    }
+}
