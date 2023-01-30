39dbc39e6212767044df64dee1aba27afff1107e
uihyeon.cha <uihyeon.cha@gmail.com>
2023-01-30 21:51:26 +0900

    fix: animation 미동작 버그 수정

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -18,7 +18,7 @@ const Backdrop: FC<Props> = (props) => {
             isDisplayed: true,
         })
 
-        window.requestAnimationFrame(() => {
+        setTimeout(() => {
             setState({
                 isVisiabled: true,
             })
