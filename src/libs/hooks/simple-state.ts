fc4b2b71d9078aa77ff459c933b694812a38bd4d
uihyeon.cha <uihyeon.cha@gmail.com>
2023-02-05 18:21:11 +0900

    feat: useReducer 에서 useState 사용하는 방식으로 변경

--- src/libs/hooks/simple-state.ts
+++ src/libs/hooks/simple-state.ts
@@ -1,14 +1,31 @@
-import { useReducer } from 'react'
+import { useEffect, useState } from 'react'
 
 /**
  * 단순한 setState
  */
-export const useSimpleState = <State = {}>(initialState: State) => {
-    return useReducer(
-        (prev: State, next: Partial<State>) => ({
-            ...prev,
-            ...next,
-        }),
-        initialState,
-    )
+export const useSimpleState = <State = {}>(
+    initialState: State,
+    onBeforeUpdate?: (prev: State, next: Partial<State>) => State,
+    onUpdated?: (state: State) => void,
+) => {
+    const [state, setState] = useState(initialState)
+
+    const dispatch = (next: Partial<State>) => {
+        setState((prev) => {
+            if (typeof onBeforeUpdate === 'function') {
+                return onBeforeUpdate(prev, next)
+            }
+
+            return {
+                ...prev,
+                ...next,
+            }
+        })
+    }
+
+    useEffect(() => {
+        onUpdated?.(state)
+    }, [state])
+
+    return [state, dispatch] as const
 }
