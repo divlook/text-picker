adf894d91c9981d12ca7469dff1f8f6ec3543e52
uihyeon.cha <uihyeon.cha@gmail.com>
2024-11-21 14:21:05 +0900

    feat: update package references and ESLint configurations, add Prettier settings

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   ignorePatterns: [".eslintrc.cjs"],
-  extends: ["@repo/eslint-config/index.js"],
+  extends: ["@pkg/eslint-config/index.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
