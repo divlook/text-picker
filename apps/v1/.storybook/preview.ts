53065f2b5a48e949dd260f0a9995af5b10bfe4f0
uihyeon.cha <uihyeon.cha@gmail.com>
2024-11-29 13:07:00 +0900

    feat: add Tailwind CSS integration and create style files for components

--- apps/v1/.storybook/preview.ts
+++ apps/v1/.storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react'
 import { defineParameters } from '../src/storybook/utils'
+import '../src/tailwind.css'
 
 const preview: Preview = {
   parameters: defineParameters({
